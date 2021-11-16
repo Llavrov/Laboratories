914f40f20086c14e69610af62a7653f6f0f65478
leva <levlevlavrov@gmail.com>
2021-11-17 00:04:44 +0500

    Новые задачки

--- task24_1.py
+++ task24_1.py
@@ -1,22 +1,38 @@
-# file = open('zadanie24_2.txt', 'r').read();
+import numpy as np
 
+a = open('27-A.txt', 'r').read().split('\n')
+b = open('27-B.txt', 'r').read().split('\n')
 
-def comp(array1, array2):
-    if (array1 is None or array2 is None) or (len(array1) != len(array2)):
-        return False
-    for i in range(len(array1)):
-        if array1[i] is not None:
-            array1[i] = array1[i] ** 2
-        if (array1[i] not in array2):
-            return False
-        array2[array2.index(array1[i])] = None
-    return True
+def minSumm(array):
+    arraySize = int(array.pop(0))
+    min3 = 1000000000
+    predmin3 = 1000000000
+    ppmin3 = 1000000000
+    min2 = 1000000000
+    min1 = 1000000000
+    for i in range(arraySize):
+        item = int(array[i])
+        if (item % 3 == 2 and item < min2):
+            min2 = item
+        if (item % 3 == 1 and item < min1):
+            min1 = item
+        if (item % 3 == 0):
+            if (item < min3):
+                ppmin3 = predmin3
+                predmin3 = min3
+                min3 = item
+            elif (item < predmin3):
+                ppmin3 = predmin3
+                predmin3 = item
+            elif (item < ppmin3):
+                ppmin3 = item
+    if (min1 + min2 > predmin3 + ppmin3):
+        return predmin3 + ppmin3 + min3
+    return min1 + min2 + min3
 
+print(minSumm(a))
+print(minSumm(b))
 
-
-a = []
-b = [1]
-print(comp(a, b))
 # if __name__ == '__main__':
 #     a = 10
 
